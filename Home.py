821b7541441950f8381b05b80fbc3d3e7ff60d05
arivia20 <129972326+arivia20@users.noreply.github.com>
2023-05-01 19:40:47 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import PIL as Image
 
 
 st.header('kimia organik')
 st.subheader('anak kimia bngt')
 st.caption ("Yok belajar")
+
+gambar1 = Image.open("image/logo sds + bg.png")
+st.image(gambar1)
