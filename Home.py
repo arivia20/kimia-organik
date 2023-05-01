247a77ca4b38023e97c50fe550ddf49016b8dde5
arivia20 <129972326+arivia20@users.noreply.github.com>
2023-05-01 19:19:17 +0700

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+Soal = st.selectbox(
+    'Pilih soal',
+    ('Soal 1', 'Soal 2', 'Soal 3'))
+if Soal == "Soal 1":
+    st.write("2 + 2 adalah...")
+    col1, col2 = st.columns (2)
+    with col1:
+        opsi1 = st.button("5")
+        opsi2 = st.button("10")
+    with col2:
+        opsi3 = st.button("4")
+        opsi1 = st.button("6")
+    if opsi1:
+        st.write("Salah")
+    elif opsi2:
+        st.write("Salah")
+    elif opsi3:
+        st.write("Benar")
+        st.balloons()
+    elif opsi1:
+        st.write("Salah")
+   
+if Soal == "Soal 2":
+    st.write("2 + 3 adalah...")
